e4a437a2e9e3a3ec72030b7f72051f7f41ab13cb
AndreRibeir0 <andrebnu1@gmail.com>
2018-09-11 19:37:19 -0300

    Adicionado o EntityFrameWork e estruturas necessárias

---
+++ DAL/Mappings/TodoMapConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL.Mappings
+{
+    public class TodoMapConfig : EntityTypeConfiguration<Todo>
+    {
+        public TodoMapConfig()
+        {
+            this.ToTable("TASK");
+            this.Property(p => p.Title).IsRequired().HasMaxLength(100);
+            this.Property(p => p.Status).IsRequired();
+        }
+    }
+}
---
+++ DAL/WebApiContext.cs
@@ -0,0 +1,28 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class WebApiContext : DbContext
+    {
+        public WebApiContext() : base("WebApiDB")
+        {
+            //A linha abaixo é descomentada para deletar e criar todas as tabelas quando necessário 
+            //Database.SetInitializer(new WebApiStrategy());
+        }
+
+        public DbSet<Todo> Todo { get; set; }
+
+
+        public class WebApiStrategy : DropCreateDatabaseAlways<WebApiContext>
+        {
+
+        }
+
+    }
+}
